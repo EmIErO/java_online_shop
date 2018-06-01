e12a96f86bc7262c77e94b8ec03397cc7a364b9e
Mirosława Bryja <mira.bryja@gmail.com>
2018-06-01 11:15:03 +0200

    Implement all iteration methods in ProductIterator class

---
+++ frompythontojava/onlineshop/part1/ProductIterator.java
@@ -0,0 +1,23 @@
+import java.util.Iterator;
+
+public class ProductIterator implements Iterator<Product> {
+    private int index;
+    private List<Product> products;
+
+    private ProductIterator(List<Product> newProducts) {
+        this.index = 0;
+        this.products = newProducts;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index < products.size();
+
+    }
+
+    @Override
+    public Question next() {
+        return produsts.get(this.index++);
+    }
+    
+}
