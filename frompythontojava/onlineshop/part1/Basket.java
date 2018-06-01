04d534f1706413e1cd57a1361ed3aaec1ec5b8d2
Mirosława Bryja <mira.bryja@gmail.com>
2018-06-01 15:07:03 +0200

    Add constructor

--- frompythontojava/onlineshop/part1/Basket.java
+++ frompythontojava/onlineshop/part1/Basket.java
@@ -1,4 +1,14 @@
 import java.lang.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 
-public class Basket{}
+public class Basket{
+    private Iterator iterator;
+    private List<Product> productList;
+
+    public Basket() {
+        this.productList = new ArrayList<Product>;
+        this.iterator = new ProductIterator(productList);
+    }
+
+}
