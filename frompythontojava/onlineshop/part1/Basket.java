294a06df42c83595fbc86e7c0558dd71874d212a
Mirosława Bryja <mira.bryja@gmail.com>
2018-06-01 15:16:03 +0200

    Implement adding products to and removing products from a basket

--- frompythontojava/onlineshop/part1/Basket.java
+++ frompythontojava/onlineshop/part1/Basket.java
@@ -11,4 +11,25 @@ public class Basket{
         this.iterator = new ProductIterator(productList);
     }
 
+    public Iterator getIterator() {
+        return this.iterator;
+    }
+
+    public void addProduct(Product product) {
+        this.productList.add(product);
+    }
+
+    public Boolean removeProduct(Product product) {
+        while (this.iterator.hasNext()) {
+            Product tempProduct = iterator.next();
+            if (tempProduct.equals(product)) {
+                iterator.remove();
+                return true;
+            } else {
+                continue;
+            }
+        }
+        return false;
+    }
+
 }
