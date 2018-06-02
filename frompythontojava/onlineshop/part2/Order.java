228633d3244d8d5f8573d820a49493ddce1a40bd
Mirosława Bryja <mira.bryja@gmail.com>
2018-06-02 19:44:09 +0200

    Create new class with fields

---
+++ frompythontojava/onlineshop/part2/Order.java
@@ -0,0 +1,11 @@
+//package frompythontojava.onlineshop.part2;
+
+public class Order implements Orderable {
+
+    private int id;
+    private String status;
+
+    public static Integer numberOfOrders = 0;
+
+    public Order{}
+}
