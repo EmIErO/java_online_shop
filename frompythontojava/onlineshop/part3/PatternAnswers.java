8e8c97783b1abde9ba68f06460429d19c55cfa8b
Mirosława Bryja <mira.bryja@gmail.com>
2018-06-03 16:31:54 +0200

    Create generic class that gets pattern answers

---
+++ frompythontojava/onlineshop/part3/PatternAnswers.java
@@ -0,0 +1,22 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class PatternAnswers<E> {
+
+    private List<String> patternAns;
+
+    public PatternAnswers(List<E> list) {
+        this.patternAns = new ArrayList<String>;
+        this.createAnswerList(list);
+    } 
+
+    public List<String> getPatternAns() {
+        return this.patternAns;
+    }
+
+    public void createAnswerList(List<E> list) {
+        for (int i; i < list.size(); i++) {
+            patternAns.add(String.valueOf(i + 1));
+        }
+    }
+}
