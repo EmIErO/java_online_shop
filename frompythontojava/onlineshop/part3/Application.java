158ebcb0dfb8e7242c74d1870ccbfab8802e4a1b
Mirosława Bryja <mira.bryja@gmail.com>
2018-06-04 12:20:00 +0200

    Start creating constructor, add fields, add printing menu and getting valid user's input

--- frompythontojava/onlineshop/part3/Application.java
+++ frompythontojava/onlineshop/part3/Application.java
@@ -1,8 +1,33 @@
 package frompythontojava.onlineshop.part3;
 
+import frompythontojava.onlineshop.part1.*;
+import frompythontojava.onlineshop.part2.*;
+
+import java.util.*;
+
 public class Application {
+
+    private List<ProductCategory> categories;
+
+    private Scanner reader;
 
     public static void main(String[] args) {
 	// write your code here
     }
+
+    public Application() {}
+
+    private void printMenu(List<String> options) {
+        int number = 1;
+        for (String option: options) {
+            System.out.println(number + ". " + option);
+            number++;
+        }
+    }
+
+    private boolean validateChoosenOpt(List<String> answers; String userAnswer) {
+        return answers.contains(answer);
+    }
+
+    private List<String> createAnswerList()
 }
